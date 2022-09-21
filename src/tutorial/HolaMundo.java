0df86ed1405325bc01aebacf29bcc6b2869fe986
Manuel <Manuel@ETP131-manuel-villa-dti>
2022-09-21 13:06:53 +0200

    Mi primer commit

---
+++ src/tutorial/HolaMundo.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola CMEPPS!");
+		System.out.println("Bienvenidos al curso");
+
+	}
+
+}
