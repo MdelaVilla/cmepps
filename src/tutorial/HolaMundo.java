bc674a0704c799ed5abea553b506c6dc07ea73ba
Manuel <Manuel@ETP131-manuel-villa-dti>
2022-10-04 03:50:37 +0200

    Muestro el nombre

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
 		System.out.println("Bienvenidos al curso");
+		
+		String name="Manuel";
+		System.out.println("Hola, "+name);
 
 	}
 
